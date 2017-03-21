eb372912d0f4805f0fb4e384d61af9430d41f4d6
Elijah Williams <ejwill04@gmail.com>
2017-03-21 14:29:42 -0600

    Build boilerplate get requests

--- server.js
+++ server.js
@@ -6,29 +6,82 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('port', process.env.PORT || 3000)
-app.locals.title = 'BYOB'
-app.locals.secrets = {
-  wowowow: 'I am a banana'
-}
+// app.locals.title = 'BYOB'
+// app.locals.secrets = {
+//   wowowow: 'I am a banana'
+// }
 
-app.get('/', (request, response) => {
-  response.send('Hello World!')
+//get endpoints
+// app.get('/', (request, response) => {
+//   fs.readFile(`${__dirname}/index.html`, (err, file) => {
+//     response.send(file);
+//   });
+// });
+
+app.get('/api/v1/users', (request, response) => {
+  database('users').select()
+  .then(users => {
+    response.status(200).json(users)
+  })
+  .catch(error => {
+    console.error('error', error)
+  })
 })
 
-app.get('/api/secrets', (request, response) => {
-  const secrets = app.locals.secrets
-
-  response.json({ secrets })
+app.get('/api/v1/comments', (request, response) => {
+  database('comments').select()
+  .then(comments => {
+    response.status(200).json(comments)
+  })
+  .catch(error => {
+    console.error('error', error)
+  })
 })
 
+app.get('/api/v1/companies', (request, response) => {
+  database('companies').select()
+  .then(companies => {
+    response.status(200).json(companies)
+  })
+  .catch(error => {
+    console.error('error', error)
+  })
+})
 
-app.get('/api/secrets/:id', (request, response) => {
+app.get('/api/users/:id', (request, response) => {
   const { id } = request.params
-  const message = app.locals.secrets[id]
 
-  if (!message) { return response.sendStatus(404)  }
+  database('users').where('id', id).select()
+  .then(users => {
+    response.status(200).json(users)
+  })
+  .catch(error => {
+    console.error('error', error)
+  })
+})
 
-  response.json({ id, message })
+app.get('/api/companies/:id', (request, response) => {
+  const { id } = request.params
+
+  database('companies').where('id', id).select()
+  .then(companies => {
+    response.status(200).json(companies)
+  })
+  .catch(error => {
+    console.error('error', error)
+  })
+})
+
+app.get('/api/comments/:id', (request, response) => {
+  const { id } = request.params
+
+  database('comments').where('id', id).select()
+  .then(comments => {
+    response.status(200).json(comments)
+  })
+  .catch(error => {
+    console.error('error', error)
+  })
 })
 
 app.listen(app.get('port'), () => {
