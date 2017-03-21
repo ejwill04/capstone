734ef4ece8c2073f00dfda4cfca38856123b46a0
Elijah Williams <ejwill04@gmail.com>
2017-03-21 17:41:02 -0600

    Add created_at to comments seed

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
