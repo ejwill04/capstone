9c3a8fac8bb79c583e1ce6c672ad18e365dbb1db
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-06 16:52:21 -0600

    Add webpack config

---
+++ app/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = (
+
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        It works!
+      </div>
+    )
+  }
+}
