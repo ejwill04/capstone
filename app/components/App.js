96056954d0a86a82928518304b1e131340365cb5
Elijah Williams <ejwill04@gmail.com>
2017-04-07 15:04:02 -0600

    Get basic front-end setup

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import '../styles.scss'
 
 export default class App extends Component {
   constructor() {
     super()
-    this.state = (
+    this.state = {
+    }
+  }
 
-    )
+  componentDidMount() {
+    console.log('called did mount')
+    fetch(`http://localhost:3000/api/v1/users`, {
+      method: 'GET',
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('data: ', data)
+    })
+    .catch(err => err)
   }
 
   render() {
--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, IndexRedirect } from 'react-router'
 import App from './components/App'
 
 ReactDOM.render(
