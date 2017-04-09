644053a8e65a366d568c69e1e2b4b44cae8490af
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-09 16:22:49 -0600

    Update class names, add styles

--- app/components/CompanyProfile.js
+++ app/components/CompanyProfile.js
@@ -4,10 +4,12 @@ import React from 'react'
 const CompanyProfile = () => {
   return(
     <div className="companyprofile-container">
-      <h1 className="profile-name">Name</h1>
-      <h2 className="profile-industry">Industry</h2>
-      <h2 className="profile-techstack">Tech Stack</h2>
-      <h2 className="profile-alumni">Alumni</h2>
+      <div className="information-container">
+        <h1 className="profile-name">Name</h1>
+        <h2 className="profile-industry">Industry</h2>
+        <h2 className="profile-techstack">Tech Stack</h2>
+        <h2 className="profile-alumni">Alumni</h2>
+      </div>
       <button>Add Yourself</button>
     </div>
   )
