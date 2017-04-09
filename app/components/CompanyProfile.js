629887215663871233ba8bb7d40673abbf883229
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-09 16:03:22 -0600

    Add Company Profile component

--- app/components/CompanyProfile.js
+++ app/components/CompanyProfile.js
@@ -3,8 +3,12 @@ import React from 'react'
 
 const CompanyProfile = () => {
   return(
-    <div>
-      company profile
+    <div className="companyprofile-container">
+      <h1 className="profile-name">Name</h1>
+      <h2 className="profile-industry">Industry</h2>
+      <h2 className="profile-techstack">Tech Stack</h2>
+      <h2 className="profile-alumni">Alumni</h2>
+      <button>Add Yourself</button>
     </div>
   )
 }
