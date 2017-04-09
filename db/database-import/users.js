69069409f958317589a14300b1bc1fe9af314642
Elijah Williams <ejwill04@gmail.com>
2017-04-09 13:40:40 -0600

    Add unfinished real data

---
+++ db/database-import/users.js
@@ -0,0 +1,516 @@
+// users from the xml file Al sent us
+// to do: populate github, slack, and remote
+[
+  {
+    name: "Pat Wentz",
+    cohort: "1605",
+    companies: "[8th Light]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Heidi Hoopes",
+    cohort: "1511",
+    companies: "[Aetna]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Patrick Hardy",
+    cohort: "1602",
+    companies: "[Aetna]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ashwin Rao",
+    cohort: "1602",
+    companies: "[AlienVault]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ryan Travitz",
+    cohort: "1608",
+    companies: "[Allstate]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Gurusundesh Khalsa",
+    cohort: "1602",
+    companies: "[Allstate]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "David Stinnette",
+    cohort: "1510",
+    companies: "[Allstate]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Tyler Komoroske",
+    cohort: "1508",
+    companies: "[allstate]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Charlie Kaminer",
+    cohort: "1603",
+    companies: "[Allstate]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "MaryJane Valade",
+    cohort: "1606",
+    companies: "[Blinker]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Matthew Hecker",
+    cohort: "1507",
+    companies: "[Blue Apron]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Jeneve Parrish",
+    cohort: "1602",
+    companies: "[Brandzooka]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Dan Winter",
+    cohort: "1510",
+    companies: "[CA Technologies]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Torie Joy-Warren",
+    cohort: "1508",
+    companies: "[CareZone]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Chris Concannon",
+    cohort: "1605",
+    companies: "[Cognizant]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Paul Nguyen",
+    cohort: "1606",
+    companies: "[Cognizant]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Justin Pease",
+    cohort: "1510",
+    companies: "[Allstate]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Matt Kaufman",
+    cohort: "1608",
+    companies: "[Concept3D]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Joshua Washke",
+    cohort: "1602",
+    companies: "[Consova]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Kimiko Kano",
+    cohort: "1511",
+    companies: "[CrownPeak]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Lacey Knaff",
+    cohort: "1608",
+    companies: "[Cultivator Advertising and Design]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Kyle Misencik",
+    cohort: "1606",
+    companies: "[Cuttlesoft]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Jeff Duke",
+    cohort: "1606",
+    companies: "[Effective Inc.]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ryan Flach",
+    cohort: "1605",
+    companies: "[Elastic Suite]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Adam Hundley",
+    cohort: "1511",
+    companies: "[FareHarbor]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Lucy Fox",
+    cohort: "1603",
+    companies: "[GoSpotCheck]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Jean Joeris",
+    cohort: "1606",
+    companies: "[GoSpotCheck]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Susi Irwin",
+    cohort: "1606",
+    companies: "[Granicus]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Tommasina Miller",
+    cohort: "1605",
+    companies: "[Granicus]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Drew Thompson",
+    cohort: "1602",
+    companies: "[Granicus]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Matt Rooney",
+    cohort: "1508",
+    companies: "[Harry Fox Agency]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Kris Foss",
+    cohort: "1603",
+    companies: "[Havenly]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Jesse Spevack",
+    cohort: "1606",
+    companies: "[Ibotta]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Aaron Careaga",
+    cohort: "1508",
+    companies: "[KITE]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Brian Rippeto",
+    cohort: "1511",
+    companies: "[Looplist]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Nate Venn",
+    cohort: "1511",
+    companies: "[Marca Global]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Matt Pindell",
+    cohort: "1602",
+    companies: "[MotoLogic]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "July Tran",
+    cohort: "1511",
+    companies: "[National Renewable Energy Laboratory]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ryan Westlake",
+    cohort: "1608",
+    companies: "[Nextworld]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "James Crockett",
+    cohort: "1510",
+    companies: "[PatientsLikeMe]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Allan Blair",
+    cohort: "1511",
+    companies: "[Pinterest]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "David Whitaker",
+    cohort: "1511",
+    companies: "[Points of light]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Aaron Greenspan",
+    cohort: "1510",
+    companies: "[Rachio]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Marcella Wigg",
+    cohort: "1603",
+    companies: "[Raftelis]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Rivena Dillon",
+    cohort: "1606",
+    companies: "[RealEyes Media]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ryan Batty",
+    cohort: "1603",
+    companies: "[Semantic Arts]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Nicholas Dorans",
+    cohort: "1511",
+    companies: "[Semantic Arts]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Kami Boers",
+    cohort: "1602",
+    companies: "[Skilljar]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Adrienne Domingus",
+    cohort: "1511",
+    companies: "[Skilljar]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Julian Feliciano",
+    cohort: "1511",
+    companies: "[Springleaf Financial]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Nick Pisciotta",
+    cohort: "1603",
+    companies: "[ThoughtWorks]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ling Tran",
+    cohort: "1602",
+    companies: "[ThoughtWorks]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Blake Worsley",
+    cohort: "1606",
+    companies: "[Trelora]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ilana Corson",
+    cohort: "1603",
+    companies: "[Turing]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Brenna Martenson",
+    cohort: "1510",
+    companies: "[Turing]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Beth Sebian",
+    cohort: "1510",
+    companies: "[Turing]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ali Schlereth",
+    cohort: "1608",
+    companies: "[Turing School of Software & Design]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Taylor Moore",
+    cohort: "1511",
+    companies: "[Turing School of Software & Design]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Sal Espinosa",
+    cohort: "1602",
+    companies: "[Turing School of Software & Design]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Robbie Jaeger",
+    cohort: "1603",
+    companies: "[Turing School of Software & Design]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Roberta Doyle",
+    cohort: "",
+    companies: "[Unsplash]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "David Davydov",
+    cohort: "1606",
+    companies: "[Wazee Digital]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Ross Edfort",
+    cohort: "1508",
+    companies: "[Wazee Digital]",
+    github: "",
+    slack: "",
+    remote: ""
+  },
+  {
+    name: "Brett Hevia",
+    cohort: "1606",
+    companies: "[WillowTree Apps]",
+    github: "",
+    slack: "",
+    remote: ""
+  }
+]
