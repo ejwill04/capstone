870e987bca839721dc1b8e1581aee60c522158d4
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-11 14:32:09 -0600

    Add and set profile

--- app/helpers/AuthService.js
+++ app/helpers/AuthService.js
@@ -7,6 +7,14 @@ export default class AuthService {
   }
   _doAuthentication(authResult){
     this.setToken(authResult.idToken)
+    this.lock.getProfile(authResult.idToken, (error, profile) => {
+      if (error) {
+        console.log('Error loading the profile', error);
+      } else {
+        debugger
+        this.setProfile(profile)
+      }
+    })
   }
   login() {
     this.lock.show()
@@ -19,6 +27,14 @@ export default class AuthService {
   }
   getToken(){
     return localStorage.getItem('id_token')
+  }
+  setProfile(profile) {
+    localStorage.setItem('profile', JSON.stringify(profile))
+    // this.emit('profile_updated', profile)
+  }
+  getProfile() {
+    const profile = localStorage.getITem('profile')
+    return profile ? JSON.parse(localStorage.profile) : {}
   }
 
   logout() {
