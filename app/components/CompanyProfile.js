908597e458c779dbc37c995f88133a5da7c6c99c
Elijah Williams <ejwill04@gmail.com>
2017-04-12 15:28:14 -0600

    Add AddEmployeePopup component and update DB

--- app/components/CompanyProfile.js
+++ app/components/CompanyProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import GithubButton from './Button'
 import CompanyFooter from './CompanyFooter'
+import AddEmployeePopup from './AddEmployeePopup'
 
 export default class CompanyProfile extends Component {
   constructor() {
@@ -61,10 +62,6 @@ export default class CompanyProfile extends Component {
     }
   }
 
-  clickEvent(e) {
-    console.log(e)
-  }
-
   render() {
     let company = this.state.companyData[0] ? this.state.companyData[0] : {}
     return (
@@ -75,7 +72,7 @@ export default class CompanyProfile extends Component {
             <h2 className="profile-techstack">Tech Stack: {company.tech_stack}</h2>
             <h2 className="profile-alumni">Alumni</h2>
             <div>{this.showUsers()}</div>
-            <GithubButton title='Add Yourself' />
+            <AddEmployeePopup />
           </div>
           <CompanyFooter data={company} />
         </div>
