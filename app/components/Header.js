a76f35786da0dd8004d94854b44dfe5d6904c07c
Devin Beliveau <devinmarieb@gmail.com>
2017-04-15 10:12:37 -0600

    Add correct header if user or no user

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,20 +2,41 @@ import React from 'react'
 import AddCompanyPopUp from './AddCompanyPopUp'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import GithubButton from './Button'
+import RaisedButton from 'material-ui/RaisedButton'
+const githubIcon = require('../photos/github.svg')
 import AuthService from '../helpers/AuthService'
 const auth = new AuthService('z3lAkZTSzkQjkiLGedtGuOcLRCe5czSd', 'gabitron.auth0.com')
 
 let userName = auth.getProfile()
 
-const Header = (props) => {
-  return (
-    <MuiThemeProvider>
-      <div className='header-container'>
+const checkForUser = (props)=> {
+  if(localStorage.length === 0) {
+    return (
+      <RaisedButton className='github-btn'
+        backgroundColor='#00C2D2'
+        label='Log in with'
+        labelPosition='before'
+        icon={<img className='github-img' src={ githubIcon }/>}
+        onClick={auth.login.bind(this)} />
+      )
+  } else {
+    return (
+      <section className='header-container'>
         <AddCompanyPopUp newCompanyAdded={props.newCompanyAdded}/>
         <aside className='company-user-login'>
           <p className='user-name'>{userName.name}</p>
           <GithubButton className="log-out-btn" title="Logout" handleClick={() => auth.logout()}/>
         </aside>
+      </section>
+    )
+  }
+}
+
+const Header = (props) => {
+  return (
+    <MuiThemeProvider>
+      <div>
+        {checkForUser(props)}
        </div>
     </MuiThemeProvider>
   )
