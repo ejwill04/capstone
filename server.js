23e90eb8dcec670c018ab81d4991565c38773e5d
Elijah Williams <ejwill04@gmail.com>
2017-04-17 15:41:07 -0600

    Remove unused server code

--- server.js
+++ server.js
@@ -19,20 +19,6 @@ app.use(function(req, res, next) {
  next()
 })
 
-// const express = require('express')
-// const path = require('path')
-// const port = process.env.PORT || 8080
-// const app = express()
-//
-// // serve static assets normally
-// app.use(express.static(__dirname + '/public'))
-//
-// // handle every other route with index.html, which will contain
-// // a script tag to your application's JavaScript file(s).
-// app.get('*', function (request, response){
-//   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-// })
-
 app.set('port', process.env.PORT || 3000)
 
 // get all users
@@ -509,9 +495,7 @@ app.delete('/api/v1/salaries/:id', (request, response) => {
 })
 
 app.get('/*', (request, response) => {
-  // fs.readFile(`${__dirname}/index.html`, (err, file) => {
-    response.sendFile(path.join(__dirname, 'build', 'index.html'))
-  // })
+  response.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 if (!module.parent) {
