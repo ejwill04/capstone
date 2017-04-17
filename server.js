3958d7ee9580823cfbda1259bd26fbd143e7eccd
Elijah Williams <ejwill04@gmail.com>
2017-04-17 15:03:53 -0600

    Change to * in server.js

--- server.js
+++ server.js
@@ -18,9 +18,23 @@ app.use(function(req, res, next) {
  next()
 })
 
+// const express = require('express')
+// const path = require('path')
+// const port = process.env.PORT || 8080
+// const app = express()
+//
+// // serve static assets normally
+// app.use(express.static(__dirname + '/public'))
+//
+// // handle every other route with index.html, which will contain
+// // a script tag to your application's JavaScript file(s).
+// app.get('*', function (request, response){
+//   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// })
+
 app.set('port', process.env.PORT || 3000)
 
-app.get('/', (request, response) => {
+app.get('*', (request, response) => {
   fs.readFile(`${__dirname}/index.html`, (err, file) => {
     response.send(file)
   })
