3c458779d4b0c7f085aefca08014e8085632ccea
Elijah Williams <ejwill04@gmail.com>
2017-04-18 14:11:22 -0600

    WIP - toggle delete

--- app/components/RenderCompanyComment.js
+++ app/components/RenderCompanyComment.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import DeleteButton from './DeleteButton'
+import classNames from 'classnames'
 
 import moment from 'moment'
 
@@ -7,25 +8,29 @@ export default class RenderCompanyComment extends Component {
   constructor(props) {
     super(props)
     this.state  = {
+      deleteConfirm: false,
     }
     this.renderDeleteBtn = this.renderDeleteBtn.bind(this)
-    this.confirmDelete = this.confirmDelete.bind(this)
   }
 
   componentWillReceiveProps(newProps) {
+    this.setState({ deleteConfirm: false })
     return newProps !== this.props
   }
 
-  confirmDelete() {
-
-  }
-
   renderDeleteBtn(user_id, id) {
-    if (this.props.user_id === user_id) {
+    if (this.state.deleteConfirm) {
       return (
-      <div className='delete-btn'>
-        <DeleteButton handleClick={() => this.props.deleteAComment(id, this.props.company_id)}></DeleteButton>
-      </div>
+        <div>
+          <button onClick={() => this.props.deleteAComment(id, this.props.company_id)}>Yes</button>
+          <button onClick={() => this.setState({ deleteConfirm: false })}>No</button>
+        </div>
+      )
+    } else {
+      return (
+        <div className='delete-btn'>
+          <DeleteButton handleClick={() => this.setState({ deleteConfirm: true })}></DeleteButton>
+        </div>
       )
     }
   }
@@ -37,7 +42,7 @@ export default class RenderCompanyComment extends Component {
          <div key={i} className='company-comment-wrapper'>
            <div className='company-comment-created_at'>{moment(obj.created_at).format('MMMM Do, YYYY')}</div>
            <div className='company-comment-message'>{obj.message}</div>
-           {this.renderDeleteBtn(obj.user_id, obj.id)}
+           {this.props.user_id === obj.user_id ? this.renderDeleteBtn(obj.user_id, obj.id) : null}
          </div>
        )
     }) : null
