c2209fb944963f398287dfffb92394769b9f3ab4
Devin Beliveau <devinmarieb@gmail.com>
2017-04-18 12:21:36 -0600

    Header almost done

--- app/components/Header.js
+++ app/components/Header.js
@@ -9,16 +9,19 @@ const checkForUser = (props)=> {
   if(localStorage.length === 0) {
     return (
       <section className='home-btn-container'>
-        <RaisedButton className='home-btn'
-          backgroundColor='#00C2D2'
-          label='Go Home to Login'
-          onClick={()=> document.location.href='/'} />
+        <aside className='user-home'>
+        <Button className='home-btn'
+          title='Go Home to Login'
+          handleClick={()=> document.location.href='/'} />
+        </aside>
       </section>
     )
   } else {
     return (
       <section className='header-container'>
-        <Button className='home-btn' title='Home' handleClick={()=> document.location.href='/'} />
+        <aside className='user-home'>
+          <Button className='home-btn' title='Home' handleClick={()=> document.location.href='/'} />
+        </aside>
         <aside className='company-user-login'>
           <AddCompanyPopUp newCompanyAdded={props.newCompanyAdded}/>
         </aside>
