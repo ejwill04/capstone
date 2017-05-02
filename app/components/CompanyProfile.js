2987f9cd53e94d62baef6f1a0bb87c5b54d754a1
Elijah Williams <ejwill04@gmail.com>
2017-05-02 13:39:51 -0600

    Rebuild and remove localhost

--- app/components/CompanyProfile.js
+++ app/components/CompanyProfile.js
@@ -32,7 +32,7 @@ export default class CompanyProfile extends Component {
   getCompany(newProps) {
       let company_id = newProps.company_id
       if (Number(company_id)) {
-        fetch(`http://localhost:3000/api/v1/companies/${company_id}`, {
+        fetch(`/api/v1/companies/${company_id}`, {
           method: 'GET',
         })
         .then(response => response.json())
