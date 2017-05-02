abec642f49da302271edf1218abafdcaf74c6dc0
Elijah Williams <ejwill04@gmail.com>
2017-05-02 12:04:37 -0600

    Update linting on ProfileDetails

--- app/components/ProfileDetails.js
+++ app/components/ProfileDetails.js
@@ -1,15 +1,14 @@
 import React, { PropTypes as T } from 'react'
-import {Row, Col, Image} from 'react-bootstrap'
+import { Row, Col, Image } from 'react-bootstrap'
 
 export class ProfileDetails extends React.Component {
-
 
   render() {
     const { profile } = this.props
     return (
       <Row>
         <Col md={2} mdOffset={4}>
-          <Image src={profile.picture} circle/>
+          <Image src={profile.picture} circle />
         </Col>
         <Col md={6}>
           <h3>Profile</h3>
